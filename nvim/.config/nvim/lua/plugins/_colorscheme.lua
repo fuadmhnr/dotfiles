9bf4c335ee7a27fd75dac7acc8830403f55ac735
fuadmhnr <fuadmhnr@gmail.com>
2025-02-27 05:58:45 +0700

    [feat] add lualine on neovim

--- nvim/.config/nvim/lua/plugins/_colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/_colorscheme.lua
@@ -3,6 +3,9 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000, -- Ensure it loads early
+    opts = {
+      flavour = "mocha"
+    },
     config = function()
       vim.cmd("colorscheme catppuccin")
     end,
