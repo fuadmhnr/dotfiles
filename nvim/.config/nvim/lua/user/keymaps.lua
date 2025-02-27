8b16be8deaa9a6b49ae183c0a9373af272207f58
fuadmhnr <fuadmhnr@gmail.com>
2025-02-27 09:15:55 +0700

    [feat] comment nvim for neovim

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -1,5 +1,5 @@
 local keymap = vim.api.nvim_set_keymap
-local options = {noremap = true, silent = true}
+local options = { noremap = true, silent = true }
 
 keymap("", "<Space>", "<Nop>", options)
 vim.g.mapleader = " "
@@ -14,17 +14,35 @@ vim.g.maplocalleader = " "
 -- command mode = "c"
 -- alt key = "m"
 
-
 -- universal mode
-for _, mode in ipairs({"i", "v", "n", "x" }) do
-  -- duplicate line
-  keymap(mode, "<S-Down>", "<CMD>t.<CR>", options)
-  keymap(mode, "<S-Up>", "<CMD>t -1<CR>", options)
+for _, mode in ipairs({ "i", "v", "n", "x" }) do
+	-- duplicate line
+	keymap(mode, "<S-Down>", "<CMD>t.<CR>", options)
+	keymap(mode, "<S-Up>", "<CMD>t -1<CR>", options)
 
-  -- Move line
-  keymap(mode, "<M-Down>", "<CMD>m+<CR>", options)
-  keymap(mode, "<M-Up>", "<CMD>m-2<CR>", options)
+	-- save file
+	keymap(mode, "<C-s>", "<cmd>silent! w<cr>", options)
 end
+
+-- duplicate line visual block
+keymap("x", "<S-Down>", ":'<,'>t'><cr>", options)
+keymap("x", "<S-Up>", ":'<,'>t-1<cr>", options)
+-- move text up and down
+keymap("x", "<A-Down>", ":move '>+1<CR>gv-gv", options)
+keymap("x", "<A-Up>", ":move '<-2<CR>gv-gv", options)
+keymap("n", "<M-Down>", "<cmd>m+<cr>", options)
+keymap("i", "<M-Down>", "<cmd>m+<cr>", options)
+keymap("n", "<M-Up>", "<cmd>m-2<cr>", options)
+keymap("i", "<M-Up>", "<cmd>m-2<cr>", options)
+-- create comment CTRL + / all mode
+keymap("v", "<C-_>", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", options)
+keymap("v", "<C-/>", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", options)
+keymap("i", "<C-_>", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", options)
+keymap("i", "<C-/>", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", options)
+keymap("i", "<C-_>", "<esc><cmd>lua require('Comment.api').toggle.linewise.current()<cr>", options)
+keymap("i", "<C-/>", "<esc><cmd>lua require('Comment.api').toggle.linewise.current()<cr>", options)
+keymap("n", "<C-_>", "<esc><cmd>lua require('Comment.api').toggle.linewise.current()<cr>", options)
+keymap("n", "<C-/>", "<esc><cmd>lua require('Comment.api').toggle.linewise.current()<cr>", options)
 
 -- close windows
 keymap("n", "<leader>q", "<CMD>q<CR>", options)
