12866c5838c25ff0b2021e7fcce4138a41f6f117
fuadmhnr <fuadmhnr@gmail.com>
2025-02-27 10:24:56 +0700

    [feat] setup bigfile, dap, colorizer, illuminate, devcolor

--- nvim/.config/nvim/lua/config/lazy_lib.lua
+++ nvim/.config/nvim/lua/config/lazy_lib.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
@@ -23,42 +23,43 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-  },
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+		{ import = "plugins.extras" },
+	},
 
-  ui = {
-    backdrop = 100, -- setting backdrop UI
-    border = "rounded", -- set border UI to rounded
-    browser = "firefox", -- default browser set
-    throttle = 40,
-    custom_keys = {
-      ["<localleader>l"] = false,
-    }
-  },
+	ui = {
+		backdrop = 100, -- setting backdrop UI
+		border = "rounded", -- set border UI to rounded
+		browser = "firefox", -- default browser set
+		throttle = 40,
+		custom_keys = {
+			["<localleader>l"] = false,
+		},
+	},
 
-  change_detection = {
-    enabled = false,
-    notify = false,
-  },
+	change_detection = {
+		enabled = false,
+		notify = false,
+	},
 
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 
-  performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
+	performance = {
+		rtp = {
+			-- disable some rtp plugins
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
